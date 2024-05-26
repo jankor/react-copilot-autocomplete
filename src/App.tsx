b98ee77d3c1e31b2cb0f46bf93cbc83bfca60477
Jan Korenek <jankorenek@live.com>
2024-05-26 20:24:40 +0200

    Add preview

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
     <div style={{width: '400px', margin: '0 auto'}}>
       <div>
         <h1>Autocomplete Area</h1>
-        <p>Type Berlin, London, Paris, Madrid, Budapest to see suggestions</p>
+        <p>Type Berlin, London, Paris, Madrid, Budapest to see suggestions <span style={{color: '#3f3f3f'}}>(use Tab on keyboard or tap the textarea on mobile to autocomplete)</span></p>
         <AutocompleteArea  dictionary={['Berlin', 'London', 'Paris', 'Madrid', 'Budapest']}/>        
       </div>
     </div>
